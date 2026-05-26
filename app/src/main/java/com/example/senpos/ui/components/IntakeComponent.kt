5af8516bdee6df8a7766e12d83cf82cb14670599
Kenneth SOARES <kensoares@gmail.com>
2026-05-26 18:44:19 +0200

    first styling of main screen

--- app/src/main/java/com/example/senpos/ui/components/IntakeComponent.kt
+++ app/src/main/java/com/example/senpos/ui/components/IntakeComponent.kt
@@ -5,12 +5,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -18,42 +20,59 @@ import androidx.compose.ui.unit.sp
 import com.example.senpos.ui.theme.SenPosTheme
 
 @Composable
-fun IntakeComponent() {
-    Surface(modifier = Modifier.fillMaxWidth()) {
-        Column (modifier = Modifier.padding(top = 16.dp, bottom = 16.dp, start = 8.dp, end = 8.dp)){
-            Row (
-                modifier = Modifier.fillMaxWidth().padding(bottom=8.dp),
+fun IntakeComponent(today: Boolean, upcoming: Boolean) {
+    Surface(modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+            color = Color(0xFFFF6C00),
+            shape = RoundedCornerShape(50.dp)
+        )
+    {
+        Column(modifier = Modifier.padding(top = 16.dp, bottom = 16.dp, start = 8.dp, end = 8.dp)) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 8.dp),
                 horizontalArrangement = Arrangement.SpaceEvenly,
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(
-                    text = "Paracétamol 500g",
+                    text = "Paracetamol 500g",
+                    color = Color(0xFFFFFFFF),
                     fontSize = 24.sp
                 )
 
                 Text(
-                    text = "1 comprimé",
+                    text = "1 pill",
+                    color = Color(0xFFFFFFFF),
                     fontSize = 20.sp,
                     fontStyle = FontStyle.Italic
                 )
             }
 
-            Row (
+            Row(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceEvenly,
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Button(onClick = { /*TODO*/ }) {
-                    Text(
-                        text = "Je l'ai pris"
-                    )
+                if (!today) {
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(
+                            text = "I took it"
+                        )
+                    }
+
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(
+                            text = "I forgot"
+                        )
+                    }
+                } else {
+                    Button(onClick = { /*TODO*/ }, enabled = !upcoming) {
+                        Text(
+                            text = if (!upcoming) "I took it" else "upcoming"
+                        )
+                    }
                 }
 
-                Button(onClick = { /*TODO*/ }) {
-                    Text(
-                        text = "J'ai oublié"
-                    )
-                }
             }
 
         }
@@ -64,6 +83,6 @@ fun IntakeComponent() {
 @Preview
 fun IntakePreview() {
     SenPosTheme {
-        IntakeComponent()
+        IntakeComponent(today = true, upcoming = true)
     }
 }
